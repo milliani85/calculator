2683809ba3a9e857d3256a1322754d42a2efe0e1
Robert Miller <robertmiller85@live.co.uk>
2022-11-07 16:11:25 +0000

    Link buttons to display.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+const buttons = document.querySelector('.input-buttons-container');
+const display = document.querySelector('.display');
 
 
 
@@ -28,11 +30,34 @@ const operate = function(n1, operator, n2) {
         add(n1,n2);
     } else if(operator === '-') {
         subtract(n1,n2);
-    } else if(operator === 'x' || operator === 'X') {
+    } else if(operator === 'x') {
         multiply(n1,n2);
     } else if(operator === '/') {
         divide(n1,n2);
     }
 }
 
+
+// Function to update display.
+
+const updateDisplay = function(buttonChoice) {
+    display.innerText += buttonChoice;
+
+}
+
+
+// Select button.
+
+buttons.addEventListener('click', (e) => {
+   
+    let buttonChoice;
+    
+    if(e.target.classList.contains('input')) {
+        buttonChoice = e.target.innerText;
+    } else {
+        buttonChoice = '';    
+    }
+    
+    updateDisplay(buttonChoice);
+})
 
