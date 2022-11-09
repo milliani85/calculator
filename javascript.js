6723c52f9d7edcf6b9b41c175e1910de8ccb4b4d
Robert Miller <robertmiller85@live.co.uk>
2022-11-09 15:55:18 +0000

    Addition function will now display on the screen
    Numbers that are pressed will now store to a variable and result of the addition calculation will push to the result display

--- javascript.js
+++ javascript.js
@@ -1,13 +1,19 @@
 const buttons = document.querySelector('.input-buttons-container');
 const clearButton = document.querySelector('.clear-button');
 const displayOne = document.querySelector('.display-one');
+const displayTwo = document.querySelector('.display-two');
 
+let n1 = 0;
+let n2 = 0;
+let operator = '';
+let result;
 
 
 // Four basic math functions.
 
 const add = function(n1, n2) {
-    return n1 + n2;
+    result =  n1 + n2;
+    return result;
 }
 
 const subtract = function(n1, n2) {
@@ -39,15 +45,49 @@ const operate = function(n1, operator, n2) {
 }
 
 
-// Update display.
+// Update display one and store display in variables.
 
-const updateDisplay = function(buttonChoice) {
+const updateDisplayOne = function(buttonChoice) {
+
+    if(buttonChoice === '+' || buttonChoice === '-'
+       || buttonChoice === 'x' || buttonChoice === '÷'){
+            n1 = displayOne.innerText;
+            n1 = +n1;
+            operator = buttonChoice;
+    }
+
+    if(n1 > 0 && buttonChoice === '='){
+        n2 = displayTwo.innerText;
+        n2 = +n2;
+    }        
+
     displayOne.innerText += buttonChoice;
-
 }
 
 
-// Clear display
+// Update display two.
+
+//displayTwo.innerText = 0;
+
+const updateDisplayTwo = function(buttonChoice) {
+    
+    if(buttonChoice === '+' || buttonChoice === '-'
+    || buttonChoice === 'x' || buttonChoice === '÷'){
+        displayTwo.innerText = '';
+ }
+
+    if(!isNaN(buttonChoice)) {
+    displayTwo.innerText += buttonChoice;
+    }
+    
+    if(buttonChoice === '=') {
+        operate(n1, operator, n2);
+        displayTwo.innerText = result;
+    }
+}
+
+
+// Clear display one.
 
 clearButton.addEventListener('click', (e) => {
     location.reload();
@@ -66,8 +106,12 @@ buttons.addEventListener('click', (e) => {
         buttonChoice = '';    
     }
     
-    updateDisplay(buttonChoice);
+    updateDisplayOne(buttonChoice);
+    updateDisplayTwo(buttonChoice);
 })
+
+
+
 
 
 
