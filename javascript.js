9a91e02e33600d9d06656a6fd99b9e42a2e6390b
Robert Miller <robertmiller85@live.co.uk>
2022-11-14 19:49:22 +0000

    Cleaned up comments

--- javascript.js
+++ javascript.js
@@ -3,8 +3,8 @@ const clearButton = document.querySelector('.clear-button');
 const displayOne = document.querySelector('.display-one');
 const displayTwo = document.querySelector('.display-two');
 
-let n1 = 0;
-let n2 = 0;
+let n1;
+let n2;
 let operator = '';
 let result;
 
@@ -52,6 +52,7 @@ const operate = function(n1, operator, n2) {
 
 const updateDisplayOne = function(buttonChoice) {
 
+//If operator is pressed then assign it to operator variable and display one text to n1 variable.
     if(buttonChoice === '+' || buttonChoice === '-'
        || buttonChoice === 'x' || buttonChoice === '÷'){
             n1 = displayOne.innerText;
@@ -59,38 +60,67 @@ const updateDisplayOne = function(buttonChoice) {
             operator = buttonChoice;
     }
 
-    if(n1 > 0 && buttonChoice === '='){
-        n2 = displayTwo.innerText;
-        n2 = +n2;
-    }        
 
     displayOne.innerText += buttonChoice;
-}
 
+
+//If equals has been pressed to run operator function, then move result to n1 and upper display when an operator is pressed.
+    if(displayOne.innerText.includes('=') && (buttonChoice === '+' ||
+       buttonChoice === '-' || buttonChoice === 'x' || buttonChoice === '÷')) {
+            n1 = result;
+            displayOne.innerText = `${n1}${buttonChoice}`;
+    }
+}
+//
+if(displayOne.innerText.includes('=') && (buttonChoice === '+' ||
+       buttonChoice === '-' || buttonChoice === 'x' || buttonChoice === '÷')) {
+            n1 = result;
+            displayOne.innerText = `${n1}${buttonChoice}`;
+    }
 
 // Update display two.
 
-//displayTwo.innerText = 0;
+// displayTwo.innerText = 0;
 
 const updateDisplayTwo = function(buttonChoice) {
-    
+
+
+//Clears display 2 when an operator is pressed.
     if(buttonChoice === '+' || buttonChoice === '-'
     || buttonChoice === 'x' || buttonChoice === '÷'){
         displayTwo.innerText = '';
  }
 
-    if(!isNaN(buttonChoice)) {
+
+ // If button choice is a number then add it to display 2 and store in n2. n2 moves to n1 when an operator is pressed.
+    
+ if(!isNaN(buttonChoice)) {
     displayTwo.innerText += buttonChoice;
+    n2 = displayTwo.innerText
+    n2 = +n2;
     }
     
+
+ // If equals is pressed run operate function.   
     if(buttonChoice === '=') {
         operate(n1, operator, n2);
         displayTwo.innerText = result;
     }
+console.log(n1)
+console.log(n2)
+console.log(operator)
+
+    //
+    // if(displayOne.innerText.includes('+') && n2 !== null) {
+    //     // operate(n1, operator, n2);
+    //     console.log('hello')
+    //     // displayTwo.innerText = result;
+    //}
+    
 }
 
 
-// Clear display one.
+// Clear display.
 
 clearButton.addEventListener('click', (e) => {
     location.reload();
