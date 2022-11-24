12720615a41bad83bedec16657054b201538c9c0
Robert Miller <robertmiller85@live.co.uk>
2022-11-24 11:58:26 +0000

    Update comments and rework code to allow second operator choice to provide calculation
    Rework code to store n1 and n2 in a different way. The reworked code allows for a second operator choice to complete the calculation

--- javascript.js
+++ javascript.js
@@ -3,9 +3,10 @@ const clearButton = document.querySelector('.clear-button');
 const displayOne = document.querySelector('.display-one');
 const displayTwo = document.querySelector('.display-two');
 
-let n1;
-let n2;
-let operator = '';
+let n1 = '';
+let n2 = '';
+let operator;
+let previousOperator;
 let result;
 
 
@@ -32,7 +33,7 @@ const divide = function(n1, n2) {
 }
 
 
-// Operator function.
+// Operator function for = button choice.
 
 const operate = function(n1, operator, n2) {
 
@@ -48,76 +49,110 @@ const operate = function(n1, operator, n2) {
 }
 
 
-// Update display one and store display in variables.
+//Operator function for operator button choice.
+const operateTwo = function(n1, previousOperator, n2) {
 
-const updateDisplayOne = function(buttonChoice) {
-
-//If operator is pressed then assign it to operator variable and display one text to n1 variable.
-    if(buttonChoice === '+' || buttonChoice === '-'
-       || buttonChoice === 'x' || buttonChoice === '÷'){
-            n1 = displayOne.innerText;
-            n1 = +n1;
-            operator = buttonChoice;
+    if(previousOperator === '+') {
+        add(n1,n2);
+    } else if(previousOperator === '-') {
+        subtract(n1,n2);
+    } else if(previousOperator === 'x') {
+        multiply(n1,n2);
+    } else if(previousOperator === '÷') {
+        divide(n1,n2);
     }
+}
 
 
-    displayOne.innerText += buttonChoice;
+// Update display one and store display in variables.
+    const updateDisplayOne = function(buttonChoice) {
+        displayOne.innerText += buttonChoice;
+ 
+        
+// Updates operator variable and previous operator variable.      
+    if(buttonChoice === '+' || buttonChoice === '-'
+        || buttonChoice === 'x' || buttonChoice === '÷'){
+            previousOperator = operator;
+            operator = buttonChoice;
+     }
+    
 
 
-//If equals has been pressed to run operator function, then move result to n1 and upper display when an operator is pressed.
+// Updates n1 and n2 variables.    
+    if(operator === undefined) {
+        n1 += buttonChoice;
+        n1 = +n1;
+    } else if (!isNaN(buttonChoice) && (displayOne.innerText.includes('+') || displayOne.innerText.includes('-') ||
+               displayOne.innerText.includes('x') || displayOne.innerText.includes('÷'))) {
+                
+        n2 += buttonChoice;
+        n2 = +n2;
+    }
+   
+
+
+// If equals has been pressed to run operator function, then move result to n1 and upper display when an operator is pressed.
     if(displayOne.innerText.includes('=') && (buttonChoice === '+' ||
        buttonChoice === '-' || buttonChoice === 'x' || buttonChoice === '÷')) {
             n1 = result;
+            n2 = '';
             displayOne.innerText = `${n1}${buttonChoice}`;
     }
-}
-//
-if(displayOne.innerText.includes('=') && (buttonChoice === '+' ||
-       buttonChoice === '-' || buttonChoice === 'x' || buttonChoice === '÷')) {
+    
+
+
+// If n2 is empty and operator is pressed clear display 2.    
+    if(n2 === '' && (buttonChoice === '+' || buttonChoice === '-'
+    || buttonChoice === 'x' || buttonChoice === '÷')){
+        displayTwo.innerText = ''; 
+ // If display one shows an operator and n2 has a value, run operate function when second operator is pressed.       
+    } else if ((displayOne.innerText.includes('+') || displayOne.innerText.includes('-') ||
+        displayOne.innerText.includes('x') || displayOne.innerText.includes('÷')) && 
+        n2 !== '' && (buttonChoice === '+' || buttonChoice === '-' || buttonChoice === 'x' || buttonChoice === '÷')) {
+            operateTwo(n1, previousOperator, n2);
             n1 = result;
             displayOne.innerText = `${n1}${buttonChoice}`;
+            n2 = '';
+            displayTwo.innerText = result;
+}
+ 
+
+//Clears diplay two when a second operator is pressed.
+    const operatorCheck = displayOne.innerText[displayOne.innerText.length -2];
+    if(!isNaN(buttonChoice) && (operatorCheck === '+' || operatorCheck === '-' ||
+        operatorCheck === 'x' || operatorCheck === '÷' )) {
+            displayTwo.innerText = '';
     }
 
-// Update display two.
-
-// displayTwo.innerText = 0;
-
-const updateDisplayTwo = function(buttonChoice) {
 
 
-//Clears display 2 when an operator is pressed.
-    if(buttonChoice === '+' || buttonChoice === '-'
-    || buttonChoice === 'x' || buttonChoice === '÷'){
-        displayTwo.innerText = '';
- }
+// If button choice is a number then add it to display 2.
+    if(!isNaN(buttonChoice)) {
+        displayTwo.innerText += buttonChoice;
+        }
 
 
- // If button choice is a number then add it to display 2 and store in n2. n2 moves to n1 when an operator is pressed.
-    
- if(!isNaN(buttonChoice)) {
-    displayTwo.innerText += buttonChoice;
-    n2 = displayTwo.innerText
-    n2 = +n2;
-    }
-    
 
- // If equals is pressed run operate function.   
+// If equals is pressed run operate function.  
     if(buttonChoice === '=') {
         operate(n1, operator, n2);
         displayTwo.innerText = result;
     }
-console.log(n1)
-console.log(n2)
-console.log(operator)
 
-    //
-    // if(displayOne.innerText.includes('+') && n2 !== null) {
-    //     // operate(n1, operator, n2);
-    //     console.log('hello')
-    //     // displayTwo.innerText = result;
-    //}
-    
+
+ 
+
+
+    console.log(n1)
+    console.log(n2)
+    console.log(operator)
+    console.log(previousOperator)
 }
+
+
+
+    
+
 
 
 // Clear display.
@@ -140,7 +175,7 @@ buttons.addEventListener('click', (e) => {
     }
     
     updateDisplayOne(buttonChoice);
-    updateDisplayTwo(buttonChoice);
+    //updateDisplayTwo(buttonChoice);
 })
 
 
