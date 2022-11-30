6e03226e99fbb18684f2ceb4aa2e8a8b8f75de03
Robert Miller <robertmiller85@live.co.uk>
2022-11-30 16:50:01 +0000

    Add a second operator function to allow for chaining together muliple calculations

--- javascript.js
+++ javascript.js
@@ -32,7 +32,12 @@ function inputNumber(num) {
 //Add operator choice and n2 to top display.
 operatorButtons.forEach(button => {
     button.addEventListener('click', (e) => {
-        inputOperator(e.target.innerText)
+        if(n1 === '') {
+            inputOperator(e.target.innerText)
+        } else if (n1 !== '' && n2 !== '') {
+            calculate();
+            secondOperator(e.target.innerText)
+        }
     })
 })
 
@@ -47,6 +52,15 @@ function inputOperator(op) {
     console.log(n2);
     console.log(operator);
 }
+
+function secondOperator(op) {
+    n1 = n2;
+    operator = op;
+    displayOne.innerText = `${n2} ${operator}`;
+    n2 = ''
+    displayTwo.innerText = '';
+}
+
 
 
 //Runs calculate function when equals is pressed.
@@ -84,118 +98,6 @@ clearButton.addEventListener('click', (e) => {
 })
 
 
-
-// //Operator function for operator button choice.
-// const operateTwo = function(n1, previousOperator, n2) {
-
-//     if(previousOperator === '+') {
-//         add(n1,n2);
-//     } else if(previousOperator === '-') {
-//         subtract(n1,n2);
-//     } else if(previousOperator === 'x') {
-//         multiply(n1,n2);
-//     } else if(previousOperator === '÷') {
-//         divide(n1,n2);
-//     }
-// }
-    
-
-
-// // Updates n1 and n2 variables.    
-//     if(operator === undefined) {
-//         n1 += buttonChoice;
-//         n1 = +n1;
-//     } else if (!isNaN(buttonChoice) && (displayOne.innerText.includes('+') || displayOne.innerText.includes('-') ||
-//                displayOne.innerText.includes('x') || displayOne.innerText.includes('÷'))) {
-                
-//         n2 += buttonChoice;
-//         n2 = +n2;
-//     }
-   
-
-
-// // If equals has been pressed to run operator function, then move result to n1 and upper display when an operator is pressed.
-//     if(displayOne.innerText.includes('=') && (buttonChoice === '+' ||
-//        buttonChoice === '-' || buttonChoice === 'x' || buttonChoice === '÷')) {
-//             n1 = result;
-//             n2 = '';
-//             displayOne.innerText = `${n1}${buttonChoice}`;
-//     }
-    
-
-
-// // If n2 is empty and operator is pressed clear display 2.    
-//     if(n2 === '' && (buttonChoice === '+' || buttonChoice === '-'
-//     || buttonChoice === 'x' || buttonChoice === '÷')){
-//         displayTwo.innerText = ''; 
-//  // If display one shows an operator and n2 has a value, run operate function when second operator is pressed.       
-//     } else if ((displayOne.innerText.includes('+') || displayOne.innerText.includes('-') ||
-//         displayOne.innerText.includes('x') || displayOne.innerText.includes('÷')) && 
-//         n2 !== '' && (buttonChoice === '+' || buttonChoice === '-' || buttonChoice === 'x' || buttonChoice === '÷')) {
-//             operateTwo(n1, previousOperator, n2);
-//             n1 = result;
-//             displayOne.innerText = `${n1}${buttonChoice}`;
-//             n2 = '';
-//             displayTwo.innerText = result;
-// }
- 
-
-// //Clears diplay two when a second operator is pressed.
-//     const operatorCheck = displayOne.innerText[displayOne.innerText.length -2];
-//     if(!isNaN(buttonChoice) && (operatorCheck === '+' || operatorCheck === '-' ||
-//         operatorCheck === 'x' || operatorCheck === '÷' )) {
-//             displayTwo.innerText = '';
-//     }
-
-
-
-// // If button choice is a number then add it to display 2.
-//     if(!isNaN(buttonChoice)) {
-//         displayTwo.innerText += buttonChoice;
-//         }
-
-
-
-// // If equals is pressed run operate function.  
-//     if(buttonChoice === '=') {
-//         operate(n1, operator, n2);
-//         displayTwo.innerText = result;
-//     }
-
-
- 
-
-
-//     console.log(n1)
-//     console.log(n2)
-//     console.log(operator)
-//     console.log(previousOperator)
-// }
-
-
-
-    
-
-
-
-
-
-
-// // Select button.
-
-// buttons.addEventListener('click', (e) => {
-   
-//     let buttonChoice;
-    
-//     if(e.target.classList.contains('input')) {
-//         buttonChoice = e.target.innerText;
-//     } else {
-//         buttonChoice = '';    
-//     }
-    
-//     updateDisplayOne(buttonChoice);
-//     //updateDisplayTwo(buttonChoice);
-// })
 
 
 
